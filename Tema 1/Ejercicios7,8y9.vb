3e489c68faed6eb280112305a7d9f69e4422a1fd
RamonRS <celmon1@gmail.com>
2020-09-28 12:55:50 +0200

    Create Ejercicios7,8y9.vb

---
+++ Tema 1/Ejercicios7,8y9.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        'EJERCICIO 7
+        Console.WriteLine(" Ejercicio 7: ")
+        Console.WriteLine("Soy Ramón , amago de programador")
+
+        Console.WriteLine("Pulse Enter para continuar")
+        Console.ReadLine()
+        'EJERCICIO 8
+        Console.WriteLine("Ejercicio 8: ")
+
+
+        Console.WriteLine("    *    ")
+        Console.WriteLine("  *   *  ")
+        Console.WriteLine("*   *   *")
+
+        'EJERCICIO 9
+        Console.WriteLine("EJERCICIO 9:")
+        Console.Title = "RAMÓN RS"
+
+        'Cambiar color de la consola
+        Console.ForegroundColor = ConsoleColor.Red
+
+        Console.WriteLine("*****")
+        Console.WriteLine("*    *")
+        Console.WriteLine("*     *")
+        Console.WriteLine("******")
+        Console.WriteLine("*")
+        Console.WriteLine("*  *")
+        Console.WriteLine("*   *")
+        Console.WriteLine("*    *")
+
+
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
