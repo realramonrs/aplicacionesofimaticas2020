250004b364c81d19353fab61c9d7e4d21f0c5c6c
RamonRS <celmon1@gmail.com>
2020-10-30 12:58:07 +0100

    Código Tema 4

---
+++ Tema 4/01_EjemploIfElseIf.vb
@@ -0,0 +1,47 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        'Programa que lee un número y me dice cuántas digitos tiene , hasta un máximo de 6 digitos
+
+        Dim numero As Integer
+        Dim digitos As Byte
+        Dim digitosOk As Boolean = True
+        Console.WriteLine("Introduce un número: ")
+        numero = Convert.ToInt32(Console.ReadLine())
+        digitos = 0
+        If numero < 10 And numero > -10 Then
+            digitos = 1
+            ' Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        ElseIf numero < 100 And numero > -100 Then
+            digitos = 2
+            '  Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        ElseIf numero < 1000 And numero > -1000 Then
+            digitos = 3
+            ' Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        ElseIf numero < 10000 And numero > -10000 Then
+            digitos = 4
+            '  Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        ElseIf numero < 100000 And numero > -100000 Then
+            digitos = 5
+            ' Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        ElseIf numero < 1000000 And numero > -100000 Then
+            digitos = 6
+            'Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        Else
+            Console.WriteLine("No se ha podido calcular el número de digitos")
+            digitosOk = False
+        End If
+        'If digitos <> 0 Then
+        '    Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        'End If
+
+        'Segunda opción para detectar si ha entrado en el else
+        If digitosOk = True Then
+            Console.WriteLine(numero & " tiene " & digitos & " digitos")
+        End If
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ Tema 4/02_EjemploSelectCase.vb
@@ -0,0 +1,72 @@
+﻿Option Strict On
+
+Module Module1
+
+    Sub Main()
+
+
+        'Select Case --> Se utiliza mayormente cuando queremos evaluar valores concretos de una variable
+        'Se suele usar mucho para código que ofrece varias opciones para el usuario.
+        Dim ejercicio As Integer
+
+        Do
+            Console.ForegroundColor = ConsoleColor.White
+
+            Console.WriteLine("Escoja el ejercicio que desea ejecutar.")
+            Console.WriteLine()
+            Console.ForegroundColor = ConsoleColor.Green
+            Console.WriteLine("5. Programa que muestra calificación a partir de la nota.")
+            Console.WriteLine()
+            Console.WriteLine("6. Programa que suma 25 a números múltiplos de 4.")
+            Console.WriteLine()
+            Console.WriteLine("7. Programa que indica estado del agua a partir de temperatura.")
+            Console.WriteLine()
+            Console.WriteLine("-1. Salir.")
+            Console.ForegroundColor = ConsoleColor.White
+            ejercicio = Convert.ToInt32(Console.ReadLine())
+
+            Select Case ejercicio
+                Case 5
+                    'Programa el código del ejercicio 5
+                    Console.WriteLine("Diseñar un programa que lea una calificación en una variable real y decida qué nota tiene")
+                Case 6
+                    Console.WriteLine("Escribir un programa que lea un número y si ese número es múltiplo de 4, entonces le suma 25, sino si es múltiplo de 5 le suma 50 y en otro caso le suma 100. Debe mostrar el valor inicial y final del número introducido")
+                Case 7
+                    Console.WriteLine("Escribir un programa (y realizar el DFD) que evalúe el valor de una variable decimal llamada temp , y escriba uno de los siguientes mensajes dependiendo de su valor:	
+                                    HIELO: Si el valor de temp es menor que cero.
+                                    AGUA: Si el valor de temp se encuentra entre cero y 100.
+                                    VAPOR: Si el valor de temp es superior a 100.")
+                Case -1
+                    Console.WriteLine("Ha decidido salir del programa")
+                Case Else
+                    Console.WriteLine("Ha escogido una opción errónea.")
+
+            End Select
+            'Console.WriteLine("Pulsa Enter para volver a jugar.")
+            'Console.ReadLine()
+            'Console.Clear()
+        Loop While ejercicio <> -1
+
+        'Dim opcion As Char
+
+        'Console.WriteLine("a. Continuar ejecutando.")
+        'Console.WriteLine("b. Salir del programa")
+
+        'opcion = Convert.ToChar(Console.ReadLine())
+
+
+        'Select Case opcion
+        '    Case "a"c
+        '    Case "A"c
+        '        Console.WriteLine("Ha escogido continuar ejecutando.")
+        '    Case "b"c
+        '        Console.WriteLine("Ha escogido salir de programa")
+        '    Case Else
+        '        Console.WriteLine("Opción no válida.")
+        'End Select
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
