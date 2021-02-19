9307a694e57fdbb26052d6fa86e0f81c549d51e2
RamonRS <celmon1@gmail.com>
2021-02-19 13:19:04 +0100

    Ficheros

---
+++ 2ªEvaluacion/Tema 6/Ficheros01.vb
@@ -0,0 +1,35 @@
+﻿Option Strict On
+Imports System.IO
+Module Ejemplo2
+    Sub Main()
+        Dim ruta As String = Directory.GetCurrentDirectory()
+        ruta += "\datos2.txt"
+
+        Console.WriteLine("Introduce nombre cancion")
+        Dim cancion As String = Console.ReadLine()
+        Console.WriteLine("Introduce nombre grupo: ")
+        Dim cantante As String = Console.ReadLine()
+        Console.WriteLine("Introduce año de la canción")
+        Dim anho As Integer = Convert.ToInt32(Console.ReadLine())
+
+        Dim registro As String = cancion + "*" + cantante + "*" + anho.ToString()
+
+        Dim escritor As StreamWriter = New StreamWriter(ruta, True)
+        escritor.WriteLine(registro)
+
+        escritor.Close()
+
+        'Listado de canciones guardadas
+        Console.WriteLine("Introduce nombre del grupo:")
+        Dim grupo As String = Console.ReadLine()
+
+        Dim lector As StreamReader = New StreamReader(ruta)
+
+        While Not lector.EndOfStream
+            Dim registro2 As String = lector.ReadLine()
+            Console.WriteLine(registro2)
+        End While
+
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ 2ªEvaluacion/Tema 6/Ficheros02.vb
@@ -0,0 +1,33 @@
+﻿Option Strict On
+Imports System.IO
+Module Module1
+
+    Sub Main()
+        Dim ruta As String = Directory.GetCurrentDirectory()
+        ruta += "\datos.txt"
+        Console.WriteLine("Directorio de trabajo: ")
+        Console.WriteLine(ruta)
+        'Escribir datos en un fichero
+        Dim escritor As StreamWriter = New StreamWriter(ruta, True)
+        escritor.WriteLine("Información para guardar en el fichero")
+        escritor.Close()
+
+        'Recuperar información de un fichero:
+        Dim lector As StreamReader = New StreamReader(ruta)
+
+        'Leerlo todo de un golpe y guardarlo en un String
+        Dim contenido As String = lector.ReadToEnd()
+        lector.Close()
+
+        Console.WriteLine("Contenido del fichero: ")
+        Console.WriteLine(contenido)
+
+
+
+
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
