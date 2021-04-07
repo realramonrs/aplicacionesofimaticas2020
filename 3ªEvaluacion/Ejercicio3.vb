b7a5521826f197dc768558bd688d8e32fb277755
RamonRS <celmon1@gmail.com>
2021-04-07 09:22:56 +0200

    ejercicio listas

---
+++ 3ªEvaluacion/Ejercicio3.vb
@@ -0,0 +1,82 @@
+﻿Option Strict On
+Imports System.IO
+Module Module1
+
+    Sub Main()
+
+        Dim opcion As Integer
+        Dim listaPeliculas As List(Of String) = New List(Of String)()
+        Dim ruta As String = Directory.GetCurrentDirectory() + "\pelis.txt"
+
+        'Volcar datos del fichero a la lista
+        If File.Exists(ruta) Then
+            Dim lector As StreamReader = New StreamReader(ruta)
+
+            While Not lector.EndOfStream
+                listaPeliculas.Add(lector.ReadLine())
+            End While
+
+            lector.Close()
+        End If
+
+        Do
+
+
+            Console.WriteLine("Escoja la opcion: ")
+            opcion = Convert.ToInt32(Console.ReadLine())
+
+            Select Case opcion
+
+                Case 1
+                    'Mostrar peliculas
+                    For Each peli As String In listaPeliculas
+                        Console.WriteLine(peli)
+                    Next
+                Case 2
+                    'Buscar una peli con contains
+                    Dim peliBuscada As String
+                    Console.WriteLine("Intro peli que desea buscar:")
+                    peliBuscada = Console.ReadLine
+
+                    Dim posicion As Integer = listaPeliculas.IndexOf(peliBuscada)
+                    If listaPeliculas.Contains(peliBuscada) Then
+                        Console.WriteLine("La peli está en la lista , posicion: " & posicion)
+                    Else
+                        Console.WriteLine("La peli no está en la lista")
+                    End If
+
+                Case 3
+                    'Añadir elementos -->add
+                    Console.WriteLine("Introduzca una peli:")
+                    Dim peli As String = Console.ReadLine()
+                    listaPeliculas.Add(peli)
+
+                    'Añadir peli al fichero
+                    Dim escritor As StreamWriter = New StreamWriter(ruta, True)
+                    escritor.WriteLine(peli)
+                    escritor.Close()
+
+                Case 4
+                    'Eliminar peli
+                    Console.WriteLine("Intro peli que desea eliminar: ")
+                    Dim peli As String = Console.ReadLine()
+
+                    listaPeliculas.Remove(peli)
+                Case 5
+                    'Volcar lista a fichero
+                    'False para sobreescribir el fichero
+                    Dim escritor As StreamWriter = New StreamWriter(ruta, False)
+
+                    For Each p As String In listaPeliculas
+                        escritor.WriteLine(p)
+                    Next
+                    escritor.Close()
+            End Select
+
+        Loop While opcion < 9
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ 3ªEvaluacion/Ejercicio4.vb
@@ -0,0 +1,47 @@
+﻿Option Strict On
+Imports System.IO
+Module Ejercicio4
+    Sub Main()
+        'Simulación lista ejercicio 1
+        Dim lista As List(Of Integer) = New List(Of Integer)(10)
+        Dim generator As Random = New Random()
+
+        For i As Integer = 10 To 19 Step 1
+            lista.Add(generator.Next(1, 51))
+        Next
+
+        Console.WriteLine("Lista original : ")
+        'Mostrar la lista creada
+        For Each numero As Integer In lista
+            Console.Write(numero & " ")
+        Next
+        Console.WriteLine()
+
+        'Empezamos aquí el ejercicio 2:
+        Dim lista2 As List(Of Integer) = New List(Of Integer)(lista)
+
+
+        For i As Integer = 0 To 4 Step 1
+            lista2.Add(generator.Next(100, 201))
+        Next
+
+        'Mostrar la lista 2
+
+        For Each elemento As Integer In lista2
+            Console.Write(elemento & " ")
+        Next
+        Console.WriteLine()
+
+
+
+
+
+
+
+
+
+
+
+        Console.ReadLine()
+    End Sub
+End Module
