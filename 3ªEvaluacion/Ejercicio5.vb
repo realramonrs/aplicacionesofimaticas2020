e0fafc1f1e22c82a1dc0d8fc113a8be0879e38a1
RamonRS <celmon1@gmail.com>
2021-04-09 13:39:41 +0200

    Listas

---
+++ 3ªEvaluacion/Ejercicio5.vb
@@ -0,0 +1,72 @@
+﻿Option Strict On
+Imports System.IO
+Module Ejercicio5
+    Sub Main()
+        Dim lista As List(Of Integer) = New List(Of Integer)(20)
+        Dim generator As Random = New Random()
+
+        For i As Integer = 0 To 19 Step 1
+            lista.Add(generator.Next(1, 26))
+        Next
+
+
+        'Vuelco a un HashSet
+        Dim conjunto As HashSet(Of Integer) = New HashSet(Of Integer)(lista)
+
+        For Each valor As Integer In conjunto
+            Console.Write(valor & " ")
+        Next
+        'Mostrar lista de números generadors
+        For Each numero As Integer In lista
+            Console.Write(numero & " ")
+        Next
+        Console.WriteLine()
+        'Obtener la posición del primer valor 2
+        Dim posicion2 As Integer = lista.IndexOf(2)
+
+        If posicion2 < 0 Then
+            Console.WriteLine("No hay número 2 en la lista")
+        Else
+            Console.WriteLine("El primer 2 está en la posición: " & posicion2)
+        End If
+
+        lista.Add(2)
+        lista.Add(2)
+        'Buscar todos los 2 de la lista
+        Dim encontrado As Boolean = False
+        For i As Integer = 0 To lista.Count - 1 Step 1
+            If lista(i) = 2 Then
+                Console.WriteLine("Posicion: " & i)
+                encontrado = True
+            End If
+        Next
+
+        If Not encontrado Then
+            Console.WriteLine("No hay número 2 en la lista.")
+
+        End If
+
+        Dim suma As Integer = 0
+        Dim media As Double = 0
+        Dim porcentaje As Double = 0
+
+        For Each numero As Integer In lista
+            suma = suma + numero
+        Next
+
+        media = suma / lista.Count
+
+        Dim contador As Integer
+
+        For Each numero As Integer In lista
+            If numero > 20 Then
+                contador += 1
+            End If
+        Next
+
+        porcentaje = (contador / lista.Count) * 100
+
+
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ 3ªEvaluacion/Ejercicio6.vb
@@ -0,0 +1,133 @@
+﻿Option Strict On
+Imports System.IO
+Module Ejercicio6
+    Sub Main()
+        Dim lista As List(Of String) = New List(Of String)()
+        Dim completadas As List(Of String) = New List(Of String)()
+        'Volcar registros a la lista
+        Dim ruta As String = Directory.GetCurrentDirectory() + "\tareas.txt"
+        Dim ruta2 As String = Directory.GetCurrentDirectory() + "\completadas.txt"
+        If File.Exists(ruta) Then
+            Dim lector As StreamReader = New StreamReader(ruta)
+
+            While Not lector.EndOfStream
+                lista.Add(lector.ReadLine())
+            End While
+            lector.Close()
+        End If
+
+        If File.Exists(ruta2) Then
+            Dim lector As StreamReader = New StreamReader(ruta2)
+
+            While Not lector.EndOfStream
+                completadas.Add(lector.ReadLine())
+            End While
+            lector.Close()
+        End If
+
+        Dim opcion As Integer
+        Do
+
+            'Obtener fecha actual
+            '  Dim hoy As Date = Date.Now
+
+
+
+
+
+
+            Console.WriteLine("Escoja la opción: ")
+            Console.WriteLine("1. Añadir tarea")
+            Console.WriteLine("2. Completar tarea.")
+            Console.WriteLine("4. Mostrar tareas pendientes.")
+            Console.WriteLine("9. Guardar cambios.")
+            opcion = Convert.ToInt32(Console.ReadLine())
+
+            Select Case opcion
+                Case 1
+                    Console.WriteLine("1. Introducir tarea")
+                    Console.WriteLine("Título:")
+                    Dim titulo As String = Console.ReadLine()
+
+                    Console.WriteLine("Prioridad: 1 - 10 (Muy prioritario")
+                    Dim prioridad As Integer = Convert.ToInt32(Console.ReadLine())
+
+                    Dim dia, mes, anho As Integer
+                    Console.WriteLine("Dia : ")
+                    dia = Convert.ToInt32(Console.ReadLine())
+                    Console.WriteLine("Mes: ")
+                    mes = Convert.ToInt32(Console.ReadLine())
+                    Console.WriteLine("Año : ")
+                    anho = Convert.ToInt32(Console.ReadLine())
+
+                    Dim fechaVencimiento As Date = New Date(anho, mes, dia)
+                    Dim registro As String = titulo + "*" + prioridad.ToString + "*" + fechaVencimiento.ToShortDateString()
+                    'Guardar el registro
+                    lista.Add(registro)
+                Case 2
+                    'Buscar la tarea que quiero completar
+                    Dim nombre As String
+                    Console.WriteLine("Introduzca nombre tarea a elimnar.")
+                    nombre = Console.ReadLine()
+
+                    For i As Integer = 0 To lista.Count - 1 Step 1
+                        If lista(i).Contains(nombre) Then
+                            'Pasar la tarea a completadas
+                            completadas.Add(lista(i))
+                            lista.RemoveAt(i)
+                            Exit For
+                        End If
+                    Next
+
+                Case 4
+                    'Mostrar tareas
+                    For Each tarea As String In lista
+                        Console.WriteLine(tarea)
+                    Next
+                Case 5
+                    'Mostrar tareas completadas
+                    For Each tarea As String In completadas
+                        Console.WriteLine(tarea)
+                    Next
+
+                Case 6
+                    'Mostrar fechas que vencen hoy
+                    Dim hoy As String = Date.Today.ToShortDateString
+
+                    For Each tarea As String In lista
+                        'Obtener fecha de vencimiento
+                        'titulo*prioridad*fecha
+                        Dim trozos() As String = tarea.Split("*"c)
+                        Dim fechaVenci As String = trozos(2)
+
+                        If fechaVenci = hoy Then
+                            Console.WriteLine(tarea)
+                        End If
+
+                    Next
+
+
+
+
+
+                Case 9
+                    Dim escritor As StreamWriter = New StreamWriter(ruta, False)
+
+                    For Each tarea As String In lista
+                        escritor.WriteLine(tarea)
+                    Next
+                    escritor.Close()
+
+                    Dim escritor2 As StreamWriter = New StreamWriter(ruta2, False)
+
+                    For Each tarea As String In completadas
+                        escritor2.WriteLine(tarea)
+                    Next
+                    escritor2.Close()
+
+            End Select
+
+        Loop While opcion < 10
+        Console.ReadLine()
+    End Sub
+End Module
